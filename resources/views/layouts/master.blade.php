8a171c8a4b121ab8f42b83092c3dcd331c6e85b6
hugorebel1998 <hugorebel1998@gmail.com>
2021-10-26 18:50:13 -0500

    Testear

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -134,33 +134,7 @@
     <div class="contact-imfo-box">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <i class="fa fa-volume-control-phone"></i>
-                    <div class="overflow-hidden">
-                        <h4>Phone</h4>
-                        <p class="lead">
-                            +01 123-456-4590
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <i class="fa fa-envelope"></i>
-                    <div class="overflow-hidden">
-                        <h4>Email</h4>
-                        <p class="lead">
-                            yourmail@gmail.com
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <i class="fa fa-map-marker"></i>
-                    <div class="overflow-hidden">
-                        <h4>Location</h4>
-                        <p class="lead">
-                            800, Lorem Street, US
-                        </p>
-                    </div>
-                </div>
+                <!--Linea de color-->
             </div>
         </div>
     </div>
@@ -181,31 +155,31 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6">
-                    <h3>About Us</h3>
+                    <h3>Sobre nosotros</h3>
                     <p>Integer cursus scelerisque ipsum id efficitur. Donec a dui fringilla, gravida lorem ac, semper magna. Aenean rhoncus ac lectus a interdum. Vivamus semper posuere dui, at ornare turpis ultrices sit amet. Nulla cursus lorem ut nisi porta,
                         ac eleifend arcu ultrices.</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h3>Opening hours</h3>
+                    <h3>Horario de apertura</h3>
                     <p><span class="text-color">Monday: </span>Closed</p>
                     <p><span class="text-color">Tue-Wed :</span> 9:Am - 10PM</p>
                     <p><span class="text-color">Thu-Fri :</span> 9:Am - 10PM</p>
                     <p><span class="text-color">Sat-Sun :</span> 5:PM - 10PM</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h3>Contact information</h3>
+                    <h3>Contacto</h3>
                     <p class="lead">Ipsum Street, Lorem Tower, MO, Columbia, 508000</p>
                     <p class="lead"><a href="#">+01 2000 800 9999</a></p>
                     <p><a href="#"> info@admin.com</a></p>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h3>Subscribe</h3>
+                    <h3>Redes sociales</h3>
                     <div class="subscribe_form">
-                        <form class="subscribe_form">
+                        {{-- <form class="subscribe_form">
                             <input name="EMAIL" id="subs-email" class="form_input" placeholder="Email Address..." type="email">
                             <button type="submit" class="submit">SUBSCRIBE</button>
                             <div class="clearfix"></div>
-                        </form>
+                        </form> --}}
                     </div>
                     <ul class="list-inline f-social">
                         <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>

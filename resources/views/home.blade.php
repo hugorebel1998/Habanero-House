c20228101756abbe783c891757448c5f97cc2155
hugorebel1998 <hugorebel1998@gmail.com>
2021-10-26 15:45:18 -0500

    Vista home falta foton de scroll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,44 +2,43 @@
 @section('title', 'Inicio')
 @section('content')
 
+
+
 <!-- Start slides -->
 <div id="slides" class="cover-slides">
     <ul class="slides-container">
         <li class="text-center">
-            <img src="{{ asset('img/slider-01.jpg')}} " alt="">
+            <img src="{{ asset('img/home/slider-01.jpg')}} " alt="">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-                        trends to see any changes in performance over time.</p>
-                        <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+                        <h1 class="m-b-20"><strong>Bienvenido a <br> Habanero House</strong></h1>
+                        <p class="m-b-40">AUTENTICA COMIDA YUCATECA.</p>
+                        {{-- <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p> --}}
                     </div>
                 </div>
             </div>
         </li>
         <li class="text-center">
-            <img src="{{ asset('img/slider-02.jpg')}}" alt="">
+            <img src="{{ asset('img/home/slider-02.jpg')}}" alt="">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-                        trends to see any changes in performance over time.</p>
-                        <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+                        <h1 class="m-b-20"><strong>Bienvenido a <br> Habanero House</strong></h1>
+                        <p class="m-b-40">AUTENTICA COMIDA YUCATECA.</p>
+                        {{-- <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p> --}}
                     </div>
                 </div>
             </div>
         </li>
         <li class="text-center">
-            <img src="{{ asset('img/slider-03.jpg')}}" alt="">
+            <img src="{{ asset('img/home/slider-03.jpg')}}" alt="">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-                        trends to see any changes in performance over time.</p>
-                        <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+                        <h1 class="m-b-20"><strong>Bienvenido a <br> Habanero House</strong></h1>
+                        <p class="m-b-40">AUTENTICA COMIDA YUCATECA.</p>
+                        {{-- <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p> --}}
                     </div>
                 </div>
             </div>
@@ -52,7 +51,232 @@
 </div>
 <!-- End slides -->
 
+<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<img src="{{ asset('img/home/nosotros.jpg')}}" alt="" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="inner-column">
+						<h1>Bienvenido a <span>Habanero House</span></h1>
+						<h2>Edward Stanley</h2>
+						<p>!Aquellos que piensan que no tienen tiempo para una alimentación saludable tarde o temprano encontrarán tiempo para la enfermedad. </p>
+						
+						{{-- <a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a> --}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
 
+    <!-- Start QT -->
+	<div class="qt-box qt-background">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 ml-auto mr-auto text-left">
+					<p class="lead ">
+						"Cualquiera puede hacerte disfrutar del primer bocado de un plato, pero sólo un verdadero chef puede hacerte disfrutar del último"
+					</p>
+					<span class="lead">-Francois Minot</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End QT -->
 
+   <!-- Start Menu -->
+	{{-- <div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Menú</h2>
+						<p>Especialidad de la casa</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="special-menu text-center">
+						<div class="button-group filter-button-group">
+							<button class="active" data-filter="*">Todo</button>
+							<button data-filter=".drinks">Bebidas</button>
+							<button data-filter=".lunch">Comidas</button>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row special-list">
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-01.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Drinks 1</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $7.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-02.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Drinks 2</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $9.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-03.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Drinks 3</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $10.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-04.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Lunch 1</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $15.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-05.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Lunch 2</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $18.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-06.jpg')}}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Lunch 3</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $20.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid dinner">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-07.jpg') }}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Dinner 1</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $25.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid dinner">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-08.jpg') }}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Dinner 2</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $22.79</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid dinner">
+					<div class="gallery-single fix">
+						<img src="{{ asset('img/home/img-09.jpg') }}" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Special Dinner 3</h4>
+							<p>Sed id magna vitae eros sagittis euismod.</p>
+							<h5> $24.79</h5>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div> --}}
+	<!-- End Menu -->
 
+    <!-- Start Gallery -->
+	<div class="gallery-box">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Degustaciones de la casa</h2>
+						{{-- <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p> --}}
+					</div>
+				</div>
+			</div>
+			<div class="tz-gallery">
+				<div class="row">
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-01.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-01.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-02.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-02.jpg')}}" alt="Gallery Images" >
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-03.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-03.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-04.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-04.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-05.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-05.jpg')}}" alt="Gallery Images">
+						</a>
+					</div> 
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-06.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-06.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-07.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-07.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-08.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-08.jpg')}}" alt="Gallery Images">
+						</a>
+					</div> 
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="{{ asset('img/home/gallery-img-09.jpg')}}">
+							<img class="img-fluid" src="{{ asset('img/home/gallery-img-09.jpg')}}" alt="Gallery Images">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Gallery -->
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,15 +9,41 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Habanero House </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/contenido.css') }}" rel="stylesheet" >
-	<link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
-	<link href="{{ asset('css/superslides.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="{{ asset('css/master/style.css') }}" rel="stylesheet" >
+	<link href="{{ asset('css/master/responsive.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/master/custom.css') }}" rel="stylesheet">
+    
+<style>
+   .qt-background {
+    background: url({{ asset('img/home/qt-bg.jpg')}}) no-repeat;
+    background-size: cover;
+    padding: 100px 0;
+    background-attachment: fixed;
+    background-position: center center;
+    position: relative;
+}
+.qt-background p {
+        font-size: 40px;
+        font-weight: 300;
+        line-height: 44px;
+        color: #fff;
+        font-family: 'Arbutus Slab', serif;
+        margin-bottom: 20px;
+    }
+    
+    .qt-background span {
+        color: #fff;
+        font-size: 30px;
+        font-weight: 500;
+    }
+
+</style>
 </head>
 <body>
-<!-- Start header -->
+
     <header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
@@ -29,31 +55,23 @@
 				</button>
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">About</a></li>
+						<li class="nav-item active"><a class="nav-link" href=""><i class="fas fa-home"></i> Inicio</a></li>
+						<li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-utensils"></i> Menú</a></li>
+						<li class="nav-item"><a class="nav-link" href="#"> <i class="fas fa-anchor"></i> N osotros</a></li>
+						<li class="nav-item"><a class="nav-link" href="contact.html"><i class="far fa-id-badge"></i>Contacto</a></li>
 						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown"><i class="fas fa-sign-in-alt"></i> Login</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="#">Reservation</a>
-								<a class="dropdown-item" href="#">Stuff</a>
-								<a class="dropdown-item" href="#">Gallery</a>
+								<a class="dropdown-item" href="#"> Inisiar sesión</a>
+								<a class="dropdown-item" href="#">Registrarse</a>
 							</div>
 						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>
-							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="blog.html">blog</a>
-								<a class="dropdown-item" href="blog-details.html">blog Single</a>
-							</div>
-						</li>
-						<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+						
 					</ul>
 				</div>
 			</div>
 		</nav>
 	</header>
-<!-- End header -->
 
 
 
@@ -61,12 +79,111 @@
         @yield('content')
     </main>
 
+ <!-- Start Contact info -->
+    <div class="contact-imfo-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <i class="fa fa-volume-control-phone"></i>
+                    <div class="overflow-hidden">
+                        <h4>Phone</h4>
+                        <p class="lead">
+                            +01 123-456-4590
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <i class="fa fa-envelope"></i>
+                    <div class="overflow-hidden">
+                        <h4>Email</h4>
+                        <p class="lead">
+                            yourmail@gmail.com
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <i class="fa fa-map-marker"></i>
+                    <div class="overflow-hidden">
+                        <h4>Location</h4>
+                        <p class="lead">
+                            800, Lorem Street, US
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Contact info -->
 
-    <!-- jQuery -->
-    <script src="{{ asset('admin-lte/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('admin-lte/plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
+    <!-- Start Footer -->
+    <footer class="footer-area bg-f">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <h3>About Us</h3>
+                    <p>Integer cursus scelerisque ipsum id efficitur. Donec a dui fringilla, gravida lorem ac, semper magna. Aenean rhoncus ac lectus a interdum. Vivamus semper posuere dui, at ornare turpis ultrices sit amet. Nulla cursus lorem ut nisi porta,
+                        ac eleifend arcu ultrices.</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3>Opening hours</h3>
+                    <p><span class="text-color">Monday: </span>Closed</p>
+                    <p><span class="text-color">Tue-Wed :</span> 9:Am - 10PM</p>
+                    <p><span class="text-color">Thu-Fri :</span> 9:Am - 10PM</p>
+                    <p><span class="text-color">Sat-Sun :</span> 5:PM - 10PM</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3>Contact information</h3>
+                    <p class="lead">Ipsum Street, Lorem Tower, MO, Columbia, 508000</p>
+                    <p class="lead"><a href="#">+01 2000 800 9999</a></p>
+                    <p><a href="#"> info@admin.com</a></p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3>Subscribe</h3>
+                    <div class="subscribe_form">
+                        <form class="subscribe_form">
+                            <input name="EMAIL" id="subs-email" class="form_input" placeholder="Email Address..." type="email">
+                            <button type="submit" class="submit">SUBSCRIBE</button>
+                            <div class="clearfix"></div>
+                        </form>
+                    </div>
+                    <ul class="list-inline f-social">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="company-name">All Rights Reserved. &copy; 2018 <a href="#">Yamifood Restaurant</a> Design By :
+                            <a href="https://html.design/">html design</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!-- End Footer -->
+
+    <script src="{{ asset('js/master/jquery-3.2.1.min.js')}} "></script> 
+	<script src="{{ asset('js/master/popper.min.js')}} "></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    <!-- ALL PLUGINS -->
+	<script src="{{ asset('js/master/jquery.superslides.min.js')}} "></script>
+	<script src="{{ asset('js/master/images-loded.min.js')}}"></script>
+	<script src="{{ asset('js/master/isotope.min.js')}} "></script>
+	<script src="{{ asset('js/master/baguetteBox.min.js')}} "></script>
+	<script src="{{ asset('js/master/form-validator.min.js')}} "></script>
+    <script src="{{ asset('js/master/contact-form-script.js')}} "></script>
+    <script src="{{ asset('js/master/custom.js')}} "></script>
     
 </body>
 

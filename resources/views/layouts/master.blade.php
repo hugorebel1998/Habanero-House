976c07188f099395a405b6bc5a5f5400382dd47d
hugorebel1998 <hugorebel1998@gmail.com>
2021-11-02 20:17:01 -0600

    Movimiento es la vista clientes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -185,13 +185,8 @@
 <footer class="footer-area bg-f">
     <div class="container">
         <div class="row">
-            {{-- <div class="col-lg-3 col-md-6">
-                <h3>Sobre nosotros</h3>
-                <p>Integer cursus scelerisque ipsum id efficitur. Donec a dui fringilla, gravida lorem ac, semper magna. Aenean rhoncus ac lectus a interdum. Vivamus semper posuere dui, at ornare turpis ultrices sit amet. Nulla cursus lorem ut nisi porta,
-                    ac eleifend arcu ultrices.</p>
-            </div> --}}
             <div class="col-lg-4 col-md-4">
-                <h3>Horario</h3>
+                <h3><i class="fas fa-calendar-week"></i> Horario</h3>
                 <p>Domingo: 11:00 - 19:00</p>
                 <p>Lunes: 11:00 - 19:00</p>
                 <p>Martes: 11:00 - 19:00</p>
@@ -201,15 +196,13 @@
                 <p>Sábado: 11:00 - 19:00</p>
             </div>
             <div class="col-lg-4 col-md-4">
-                <h3>Dirección</h3>
-                <p class="lead">Sindicato Nacional de Electricistas 16, Hab Electra, 54060 Tlalnepantla de Baz, Méx</p>
+                <h3><i class="fas fa-map-marker-alt"></i> Dirección</h3>
+                <p> <a href="https://www.google.com.mx/maps/place/Sindicato+Nacional+de+Electricistas+16,+Hab+Electra,+54060+Tlalnepantla+de+Baz,+M%C3%A9x./@19.5277411,-99.2224059,17z/data=!3m1!4b1!4m5!3m4!1s0x85d21d33ca85b037:0x5cef53018627ccfe!8m2!3d19.5277361!4d-99.2202172" target="_black">Sindicato Nacional de Electricistas 16, Hab Electra, 54060 Tlalnepantla de Baz, Méx</a></p>
             </div>
             <div class="col-lg-4 col-md-4">
-                <h3>Redes sociales</h3>
+                <h3><i class="fas fa-retweet"></i> Redes sociales</h3>
                 <ul class="list-inline f-social">
                     <li class="list-inline-item"><a href="https://www.facebook.com/HabaneroHouse" target="_blank"><i class="fab fa-facebook-square test-white"></i></a></li>
-                    {{-- <li class="list-inline-item"><a href="#"><i class="fab fa-twitter-square"></i></a></li> --}}
-                    {{-- <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li> --}}
                     <li class="list-inline-item"><a href="https://www.google.com/search?q=habanero+house+&sxsrf=AOaemvIkeYptiEKGvtFafRsEXUfZk7s31A%3A1635294742924&ei=Fp54YdnpN-e5qtsP3sWU-Ao&ved=0ahUKEwjZzITSq-nzAhXnnGoFHd4iBa8Q4dUDCA4&uact=5&oq=habanero+house+&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECc6CggjELACELADECc6BwgjEOoCECc6DQguEMcBEKMCEOoCECc6BAguEEM6CAgAEIAEELEDOg4ILhCABBCxAxDHARCjAjoICAAQsQMQgwE6DgguEIAEELEDEMcBENEDOgsILhCABBDHARCjAjoECAAQQzoKCC4QxwEQrwEQJzoHCC4QsQMQQzoLCAAQgAQQsQMQgwE6EAguEIAEEIcCEMcBEK8BEBQ6CAguEIAEELEDOgUIABCABDoGCAAQChATOgYILhAKEBM6CAgAEAoQHhATOgYIABAeEBNKBAhBGAFQ3lhYt5EBYK6UAWgEcAB4AIABkweIAcovkgENMC4yLjIuMi4xLjIuM5gBAKABAbABCsgBAcABAQ&sclient=gws-wiz" target="_blank"><i class="fab fa-google-plus-square"></i></a></li>
                     <li class="list-inline-item"><a href="https://www.instagram.com/habanero_house_oficial/" target="_blank"><i class="fab fa-instagram"></i></a></li>
                 </ul>
@@ -221,7 +214,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <p><?= date('Y')?> | Habanero House:
+                    <p><?= date('Y')?> | Habanero House </p>
                 </div>
             </div>
         </div>

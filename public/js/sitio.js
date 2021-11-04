a1a5a43cf932ca4b91e4bbc4c68ca5938dfb2add
hugorebel1998 <hugorebel1998@gmail.com>
2021-11-04 00:07:16 -0600

    Spinner en el proyecto

---
+++ public/js/sitio.js
@@ -0,0 +1,10 @@
+let loader = document.getElementById('loader');
+
+
+window.onload = function () {
+    setTimeout(() => {
+        loader.style.visibility = 'hidden';
+        loader.style.display = 'none';
+    }, 1000)
+}
+
